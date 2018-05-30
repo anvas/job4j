722ae82f542bd5cffa8f367418b83cbcfcaf6a80
anvas <admsocs@yandex.ru>
2018-05-30 13:37:20 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate.
+ * @author Andrey Vasiliev (admsocs@yandex.ru)
+ */
+
 public class Calculate {
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
