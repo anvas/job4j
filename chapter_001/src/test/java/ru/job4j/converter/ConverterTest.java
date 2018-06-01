262d583c0d1eb32d7a8730cc4829a504f9550433
anvas <admsocs@yandex.ru>
2018-06-01 12:26:58 +0300

    3.2. Конвертер валюты. [#41583]

---
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ *@author Andrey Vasiliev (admsocs@yandex.ru).
+ *@version $Id$
+ *@since 0.1
+ */
+
+public class ConverterTest {
+
+    /**
+     *Test converter rubleToDollar.
+     */
+    @Test
+    public void when60RubleToDollarThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToDollar(60);
+        assertThat(result, is(1));
+    }
+
+    /**
+     *Test converter rubleToEuro.
+     */
+    @Test
+    public void when70RubleToEuroThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToEuro(70);
+        assertThat(result, is(1));
+    }
+
+    /**
+     *Test converter dollarToRuble.
+     */
+    @Test
+    public void when1DollarToRubleThen60() {
+        Converter converter = new Converter();
+        int result = converter.dollarToRuble(1);
+        assertThat(result, is(60));
+    }
+
+    /**
+     *Test converter euroToRuble.
+     */
+    @Test
+    public void when1EuroToRubleThen70() {
+        Converter converter = new Converter();
+        int result = converter.euroToRuble(1);
+        assertThat(result, is(70));
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/converter/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for converter task.
+ *
+ * @author Andrey Vasiliev (admsocs@yandex.ru).
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.converter;
