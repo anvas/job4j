64575876f6d01f162ae60e4faaf824c56917ad52
anvas <admsocs@yandex.ru>
2018-06-13 11:56:07 +0300

    5.5. Рефакторинг кода. [#33459]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,5 +1,7 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * Программа - Построение пирамиды в псевдографике.
  * @author Andrey Vasiliev (admsocs@yandex.ru).
@@ -10,19 +12,30 @@ package ru.job4j.loop;
 public class Paint {
 
     /**
-     * Рисует пирамиду.
-     * @param height высота доски.
-     * @return Пирамида в псевдографике.
+     * Метод, проверяюший условие для простановки галки.
      */
     public String pyramid(int height) {
-        // Буфер для результата.
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+    /**
+     * Метод, принимающий параметры.
+     * @param height - высота пирамиды.
+     * @param weight - ширина пирамиды.
+     * @param predict - условие проставление галки.
+     * @return Пирамида в псевдографике.
+     */
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int weight = 2 * height - 1;
         // внешний цикл двигается по строкам.
         for (int row = 0; row != height; row++) {
             // внутренний цикл определяет положение ячейки в строке.
             for (int column = 0; column != weight; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
